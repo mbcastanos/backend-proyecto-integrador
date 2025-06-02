60f833bd8d2680a82036b238c8bf628f817f2bc7
Ariel Romero <aromero@alumnos.unsada.edu.ar>
2025-06-02 15:51:38 -0300

    feat:agregar controller forma_geometrica con endpoints GET y POST

--- src/app.py
+++ src/app.py
@@ -1,18 +1,29 @@
-from flask import Flask, jsonify
-from models import db, Calzado, Suela, DetalleSuela #Se importaron Suela y DetalleSuela
+from flask import Flask, jsonify, Blueprint
+from models import (
+    db,
+    Calzado,
+    Suela,
+    DetalleSuela,
+)  # Se importaron Suela y DetalleSuela
 from controllers.calzado_controller import calzado_bp
 from controllers.suela_controller import suela_bp
+from controllers.forma_geometrica_controller import forma_bp
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3306/calzado'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+mysqlconnector://root:admin@localhost:3306/huellasdb"
+)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
 app.register_blueprint(calzado_bp)
-app.register_blueprint(suela_bp)  #Se añadio el suela_bp
+app.register_blueprint(suela_bp)
+app.register_blueprint(forma_bp)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
