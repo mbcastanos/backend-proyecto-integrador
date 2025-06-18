e3c7103d0fd74b9b531ea3fe938838bb67330dbc
German <polettigerman@outlook.com>
2025-06-18 16:57:10 -0300

    agregada logica de manejo de tokens y agregados otros endpoints para el manejo de los usuarios

--- src/controllers/login_controller.py
+++ src/controllers/login_controller.py
@@ -1,7 +1,16 @@
 import datetime
+import jwt
 import bcrypt
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, Response, g, json, jsonify, request
 from models import db, Usuario
+from controllers.auth import token_required
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Para los que hagan pull: definan esta variable de entorno en un archivo .env
+# Esto es temporal hasta que se suba el codigo a un servidor
+secret_key = os.getenv("SECRET_KEY")
 
 login_bp = Blueprint('login_bp', __name__)
 
@@ -20,10 +29,18 @@ def login():
     if not usuario or not bcrypt.checkpw(password.encode(), usuario.password_hash.encode()):
         return jsonify({"error": "Credenciales incorrectas"}), 401
     
-    return jsonify({"message" : "inicio valido"}), 200
-    
+    payload = {
+        "user_id": usuario.id,
+        "username": usuario.username,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # token válido 1 hora
+    }
 
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+
+    return jsonify({"message" : "inicio valido", "token" : token}), 200
+    
 @login_bp.route("/usuarios", methods = ["POST"])
+@token_required
 def create_user():
     data = request.get_json()
     username = data.get("username")
@@ -48,6 +65,105 @@ def create_user():
 
     return jsonify({"msg": "Usuario creado exitosamente."}), 201
 
+@login_bp.route("/me", methods=["GET"])
+@token_required
+def obtener_usuario_actual():
+    usuario = Usuario.query.get(g.user["user_id"])
+    if not usuario:
+        return jsonify({"error": "Usuario no encontrado"}), 404
+
+    return jsonify({
+        "id": usuario.id,
+        "username": usuario.username,
+        "role": usuario.role
+    }), 200
+
+@login_bp.route("/usuarios/<int:user_id>", methods=["PATCH"])
+@token_required
+def actualizar_usuario(user_id):
+    data = request.get_json()
+    
+    usuario = Usuario.query.get(user_id)
+    if not usuario:
+        return jsonify({"error" : "Usuario no encontrado" }), 404
+    
+    new_username = data.get("username")
+    new_password = data.get("password")
+    new_role = data.get("role")
+
+    if new_username:
+        if Usuario.query.filter(Usuario.username==new_username, Usuario.id != user_id).first():
+            return jsonify({"error": "El nombre de usuario ya está en uso"}), 400
+        usuario.username = new_username
+    
+    if new_password:
+        usuario.password_hash = bcrypt.hashpw(new_password.encode(), bcrypt.gensalt()).decode('utf-8')
+    
+    if new_role:
+        usuario.role = new_role
 
 
+    if not new_username and not new_password and not new_role:
+        return jsonify({"error": "No se dieron datos para actualizar"}), 400
 
+    db.session.commit()
+
+    new_payload = {
+        "user_id": usuario.id,
+        "username": usuario.username,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # token válido 1 hora
+    }
+
+    new_token = jwt.encode(new_payload, secret_key, algorithm="HS256")
+
+    return jsonify({"message": "Usuario actualizado exitosamente", "new_token": new_token}), 200   
+    
+
+@login_bp.route("/usuarios/<int:id>", methods=["GET"])
+@token_required
+def get_user_data_by_id(id):
+    usuario = Usuario.query.get(id)
+    if not usuario:
+        return jsonify({"error": "Usuario no encontrado"}), 404
+        
+    return jsonify({
+        "id": usuario.id,
+        "username": usuario.username,
+        "role": usuario.role
+    }), 200  
+
+
+@login_bp.route("/usuarios/<int:id>", methods=["DELETE"])
+@token_required
+def delete_user_by_id(id):
+    usuario = Usuario.query.get(id)
+    
+    if not usuario:
+        return jsonify({"error": "Usuario no encontrado"}), 404
+    
+    db.session.delete(usuario)
+    db.session.commit()
+
+    return jsonify({"message": "Usuario eliminado exitosamente"}), 200
+
+
+@login_bp.route("/usuarios", methods=["GET"])
+@token_required
+def get_all_users():
+    
+    usuarios = Usuario.query.all()
+
+    if not usuarios:
+        return jsonify({"message": "No hay usuarios registrados"}), 404
+
+    lista = [
+        {
+            "id": usuario.id,
+            "username": usuario.username,
+            "role": usuario.role
+        }
+        for usuario in usuarios
+    ]
+
+    json_data = json.dumps(lista, ensure_ascii=False, sort_keys=False)
+    return Response(json_data, mimetype='application/json')
